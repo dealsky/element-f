bd90c37a0d93c4c8b6c635ad9a962b68b29f2040
dealsky <dealsky000@outlook.com>
2018-07-10 15:53:38 +0800

    登录页

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="login">
-    <div class="login-form">
+    <el-form class="login-form">
+      <h3 class="title">系统登录</h3>
       <div class="input-wrapper">
-        <el-input></el-input>
+        <span class="icon-user"></span>
+        <el-input class="input"></el-input>
       </div>
       <div class="input-wrapper">
-        <el-input></el-input>
+        <i class="icon-lock"></i>
+        <el-input class="input"></el-input>
       </div>
-
-      <button class="submit">提交</button>
-    </div>
+      <el-button class="btn-login" type="primary">登录</el-button>
+    </el-form>
   </div>
 </template>
 
@@ -20,9 +22,25 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import "../../common/stylus/icon.styl"
+  @import "../../common/stylus/variables.styl"
+
   .login
     .login-form
-      margin: 25% auto 0 auto
+      margin 200px auto 0 auto
+      padding 35px 35px 15px 35px
+      box-sizing border-box
+      width 520px
+      .title
+        margin-bottom 40px
+        text-align center
+        font-size 26px
       .input-wrapper
-        width
+        margin-bottom: 22px
+        .icon-user
+          display: inline-block
+        .input
+          width 85%
+      .btn-login
+        width 100%
 </style>
