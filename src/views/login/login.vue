7ee21f44adba3d6dd5dae3bed087f0fb718a1e86
dealsky <dealsky000@outlook.com>
2018-07-10 20:24:09 +0800

    登录页

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -2,15 +2,20 @@
   <div class="login">
     <el-form class="login-form">
       <h3 class="title">系统登录</h3>
-      <el-form-item class="input-wrapper">
-        <i class="icon icon-user"></i>
-        <el-input class="input" type="text" placeholder="账号"></el-input>
+
+      <el-form-item>
+        <el-input type="text" placeholder="账号">
+          <i class="icon icon-user" slot="prefix"></i>
+        </el-input>
       </el-form-item>
 
       <el-form-item class="input-wrapper">
-        <i class="icon icon-lock"></i>
-        <el-input class="input" type="password" placeholder="密码"></el-input>
+        <el-input class="input" type="password" placeholder="密码">
+          <i class="icon icon-lock" slot="prefix"></i>
+          <i class="icon-suffix icon-eye_off" slot="suffix"></i>
+        </el-input>
       </el-form-item>
+
       <el-button class="btn-login" type="primary">登录</el-button>
     </el-form>
   </div>
@@ -22,25 +27,46 @@ export default {
 };
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
   @import "../../common/stylus/icon.styl"
   @import "../../common/stylus/variables.styl"
 
   .login
+    position: fixed
+    width 100%
+    height 100%
+    background #303a49
     .login-form
-      margin 200px auto 0 auto
+      margin 120px auto 0 auto
       padding 35px 35px 15px 35px
       box-sizing border-box
-      width 520px
-      .input-wrapper
-        .icon
-          display inline-block
-        .input
-          width 85%
+      width 500px
       .title
         margin-bottom 40px
         text-align center
         font-size 26px
+        color: #eee
+      .el-form-item
+        margin-bottom: 22px
+        .el-input
+          border: 1px solid $color-border-l
+          border-radius 4px
+          input
+            background #2b3442
+            border: 0
+            color: #eee
+          .icon
+            display inline-block
+            vertical-align top
+            margin-top: 12px
+            font-size: 16px
+            color: #8b99a3
+          .icon-suffix
+            display: inline-block;
+            margin-top: 10px;
+            vertical-align: top;
+            font-size 24px
+            color: #8b99a3
       .btn-login
         width 100%
 </style>
